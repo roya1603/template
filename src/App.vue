ce679ace78fe3e8ea791b27d6c479f0a4a4463f0
royaibrahimova86 <royaibrahimova86@gmail.com>
2024-11-02 06:52:43 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<Nav/>
+<Header/>
+<About/>
+</template>
+
+<script setup>
+import Nav from "./components/NavComp.vue";
+import Header from "./components/HeaderComp.vue";
+import About from "./components/AboutComp.vue";
+</script>
